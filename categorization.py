434cc11f0157e86422cbca7da8f1f336ed093c65
Joseph Cutler <joseph_cutler@watkinson.org>
2016-07-20 12:02:19 -0400

    refactored to read from database, respond to POST requests.

--- categorization.py
+++ categorization.py
@@ -1,4 +1,11 @@
 from __future__ import division
+import json
+from flask import Flask
+from flask import request
+from flask import Response 
+from http_parser.parser import HttpParser
+from urlparse import urlparse
+from pg import DB
 import math
 from IPython import embed
 import operator
@@ -9,14 +16,13 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import nltk.data
 import string
+import sys
 from collections import Counter
 from nltk.stem.porter import *
-from sklearn.feature_extraction.text import TfidfVectorizer
+import socket
 
-
-def setup():
-    nltk.download('punkt')
-    nltk.download('stopwords')
+db = DB(dbname='issue_categorization', port=5432)
+app = Flask(__name__)
 
 def parse_text(s):
     raw_tokenized = [word for word in word_tokenize(s)]
@@ -36,24 +42,6 @@ def parse_text(s):
     words = filter(is_tick,words)
     return words
 
-
-def parse_from_file(filename):
-    with open(filename) as f:
-        md = mistune.markdown(f.read().decode('utf-8'))
-        soup = BeautifulSoup(md, 'html.parser')
-        data = soup.get_text()
-    return parse_text(data)
-
-#A word is a string, a lexicon is a collection of 2 tuples: (str,count)
-def is_member_of(word,lex):
-    if lex[word] > 0:
-        return 1
-    else:
-        return 0
-
-#def weight(word,lex):
-#    return math.sqrt(lex[word])/(len(lex) + 1)
-
 def msum(f,l):
     sig = 0
     for x in l:
@@ -66,82 +54,69 @@ def mproduct(f,l):
         pr *= f(x)
     return pr
 
-
 class Lexicon:
-    def __init__(self,stems):
-        self._stems = dict(Counter(stems))
+    def __init__(self,name):
+        self.name = name
+        self.size = db.query("SELECT count(*) from {:s}_words".format(name)).getresult()[0][0]
 
     def has_member(self,s):
-        if s in self._stems:
-            return 1
+        result = db.get_as_list("{:s}_words".format(self.name),where="word='{:s}'".format(s))
+        if result != []:
+            return True
+        else:
+            return False
+
+    def get_raw_count(self,word):
+        result = db.get_as_list("{:s}_words".format(self.name),where="word='{:s}'".format(word))
+        if result != []:
+            return result[0][2]
         else:
             return 0
+
 
     def weight(self, word):
-        if self.has_member(word):
-            return math.sqrt(self._stems[word]/(len(self._stems) + 1))
+        if self.has_member(word) == 1:
+            w = math.sqrt(self.get_raw_count(word)/(self.size + 1))
+            return w
         else:
             return 0
-        
 
-
-
-if __name__ == '__main__':
-    #issue_text = "I completed the lab using the 'learn -b' command from the CLI and got all tests passing. Then I ran just 'learn' to check it in and it said \"Owner should be able to go on a bike ride\" fails, even though this was done and repeated within seconds of each other. I'll open a PR for the code in a moment."
-    #issue_text = "Use of 'optional' here is confusing because recommended reading has section which refers to \"Optional Arguments\" (defined in method signature with *) which are different than \"Arguments with Default Values\" (also covered in the recommended reading) which is what we appear to be going for here. "
-    #issue_text = "The video and the readme can really be broken in half.\nFirst, the mechanics of defining methods.\nThen arguments."
-    #issue_text = "Wouldn't this example method stop removing apples from the basket when there were 5 apples in the basket and 5 taken out instead of continuing until all the apples were removed from the basket? Because at that point the condition for continuing to iterate would be false---5 is not less than 5. Or am I just misunderstanding something? Thanks!"
-    #issue_text = "Description for #non_sailors spec is wrong and should be people who are not captains of sail boats"
-    #issue_text = """
-    #Break apart text readings into separate lessons and relink them in learn as separate repos.
-    #"""
-    issue_text = """
-    Update CocoaPods, only Specta/Expecta needed
-
-    Expecta not imported into Spec file
-
-    Advanced section introduces NSError. Is this too early? Nice link to the NSHipster blog post.
-
-    Add objectives and update for consistent style.
-    """
-
-    issue_text = """
-    "i reached the first lab lesson, but i find that the \"open\" tab is greyed and cannot be clicked to open the CLI in introus. when i manually open nitrous CLI IDE, i can't find the lesson folder under the lab folder, and i keep getting message \"You don't appear to be in a Learn lesson's directory. Please cd to an appropriate directory and try again.\"",
-    """
-
-    setup()
-    impl = Lexicon(parse_from_file("impl.txt"))
-    prod = Lexicon(parse_from_file("prod.txt"))
-    test = Lexicon(parse_from_file("test.txt"))
-
-    #lexicons = [impl,prod,test]
+def get_issue_category(issue, lexicons):
+    impl,prod,test = lexicons
     D = set([impl,prod,test])
-    
-    print("Testing issue \"{:s}...\"".format(issue_text[:20]))
 
-    issue_words = parse_text(issue_text)
+    issue_words = parse_text(issue)
 
     prod_score = msum(lambda w : (
         len(D) * prod.weight(w) / (msum(lambda lex: lex.weight(w), D - set([prod])) + 1)
-    ),issue_words)
+        ),issue_words)
 
     test_score = msum(lambda w : (
         len(D) * test.weight(w) / (msum(lambda lex: lex.weight(w), D - set([test])) + 1)
-    ),issue_words)
+        ),issue_words)
 
     impl_score = msum(lambda w : (
         len(D) * impl.weight(w) / (msum(lambda lex: lex.weight(w), D - set([impl])) + 1)
-    ),issue_words)
+        ),issue_words)
 
 
     issue_scores = {
             'test': test_score,
             'impl': impl_score,
             'prod': prod_score
-    }
+            }
 
-    category = max(issue_scores, key=issue_scores.get)
-    print("Issue is most likely a(n) {:s} issue".format(category))
-    print(issue_scores)
-    #embed()
+    return max(issue_scores, key=issue_scores.get)
 
+@app.route('/',methods=['POST'])
+def issue_request():
+    impl = Lexicon("impl")
+    prod = Lexicon("prod")
+    test = Lexicon("test")
+
+    raw_data = request.data
+    if not raw_data:
+        return Response("400 Bad Request")
+
+    issue_json = json.loads(raw_data)
+    return Response(get_issue_category(issue_json['issue'],(impl,prod,test)))
